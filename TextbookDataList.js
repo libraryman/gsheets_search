fd770ca966cd0522183eeed89c93b165bc12cb6b
libraryman <jeffreystephensabol@gmail.com>
2018-07-25 09:36:27 -0700

    added fake data

---
+++ TextbookDataList.js
@@ -0,0 +1,69 @@
+let TextbookDataList = [
+  {
+    title: "Book One",
+    author: "author One",
+    class:"Class One",
+    professor:"Professor One"
+  },
+  {
+    title: "Book Two",
+    author: "author Two",
+    class:"Class Two",
+    professor:"Professor Two"
+  },
+  {
+    title: "Book Three",
+    author: "author Three",
+    class:"Class Three",
+    professor:"Professor Three"
+  },
+  {
+    title: "Book Four",
+    author: "author Four",
+    class:"Class Four",
+    professor:"Professor Four"
+  },
+  {
+    title: "Book Five",
+    author: "author Five",
+    class:"Class Five",
+    professor:"Professor Five"
+  },
+  {
+    title: "Book Six",
+    author: "author Six",
+    class:"Class Six",
+    professor:"Professor Six"
+  },
+  {
+    title: "Book Seven",
+    author: "author Seven",
+    class:"Class Seven",
+    professor:"Professor Seven"
+  },
+  {
+    title: "Book Eight",
+    author: "author Eight",
+    class:"Class Eight",
+    professor:"Professor Eight"
+  },
+  {
+    title: "Book Nine",
+    author: "author Nine",
+    class:"Class Nine",
+    professor:"Professor Nine"
+  },
+  {
+    title: "Book Ten",
+    author: "author Ten",
+    class:"Class Ten",
+    professor:"Professor Ten"
+  },
+  {
+    title: "Book Eleven",
+    author: "author Eleven",
+    class:"Class Eleven",
+    professor:"Professor Eleven"
+  },
+
+];
